57721c96613d7dde4a81db689290a2315b3c7c7b
Remi Gatard <remi.gatard@runtastic.com>
2025-02-25 13:22:07 +0100

    Add insertion, deletion and selection of edges
    I'm using a global exception handling @ControllerAdvice class to handle all those, but that means the controller isn't controling it, so not sure I like that

--- src/main/kotlin/fr/batgard/prewave_assignment/edge/EdgeController.kt
+++ src/main/kotlin/fr/batgard/prewave_assignment/edge/EdgeController.kt
@@ -1,45 +1,89 @@
 package fr.batgard.prewave_assignment.edge
 
 import fr.batgard.prewave_assignment.db.models.tables.Edge.Companion.EDGE
+import jakarta.servlet.http.HttpServletRequest
 import org.jooq.DSLContext
+import org.jooq.Name
 import org.jooq.exception.IntegrityConstraintViolationException
+import org.jooq.impl.DSL.*
+import org.jooq.impl.SQLDataType.INTEGER
+import org.springframework.core.annotation.AnnotationUtils
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.ModelAndView
 
 @RestController
 @RequestMapping("/edge")
 class EdgeController(private val edgeService: EdgeService) {
 
     @PostMapping
-    fun createEdge(@RequestBody requestBody: CreateEdgeRequestBody): ResponseEntity<Any> {
-        return try {
-            edgeService.createEdge(requestBody)
-            ResponseEntity.status(HttpStatus.CREATED).body("Edge created successfully.")
-        } catch (e: EdgeAlreadyExistsException) {
-            ResponseEntity.status(HttpStatus.CONFLICT).body(e.message)
-        } catch (e: Exception) {
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.")
-        }
+    fun createEdge(@RequestBody requestBody: EdgeRequestBody): ResponseEntity<Any> {
+        edgeService.createEdge(requestBody)
+        return ResponseEntity.status(HttpStatus.CREATED).body("Edge created successfully.")
+    }
+
+    @DeleteMapping
+    fun deleteEdge(@RequestBody requestBody: EdgeRequestBody): ResponseEntity<String> {
+        edgeService.deleteEdge(requestBody)
+        return ResponseEntity.ok("Edge deleted successfully.")
+    }
+
+    /**
+     * @param rootNodeId the root node ID
+     * FIXME: this needs to be paginated
+     */
+    @GetMapping
+    fun getTree(rootNodeId: Int): ResponseEntity<List<Pair<Int, Int>>> {
+        return ResponseEntity.ok(edgeService.fetchTreeWithRoot(rootNodeId))
     }
 }
 
-data class CreateEdgeRequestBody(
+@ControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(Exception::class)
+    fun defaultErrorHandler(req: HttpServletRequest, e: Exception): ModelAndView {
+        if (AnnotationUtils.findAnnotation(e.javaClass, ResponseStatus::class.java) != null) {
+            throw e
+        }
+
+        return ModelAndView("error").apply {
+            addObject("exception", e)
+            addObject("url", req.requestURL)
+        }
+    }
+
+    @ExceptionHandler(EdgeAlreadyExistsException::class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    fun handleEdgeAlreadyExistsException(e: EdgeAlreadyExistsException) {
+    }
+
+    @ExceptionHandler(EdgeNotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleEdgeNotFoundException(e: EdgeAlreadyExistsException) {
+    }
+}
+
+data class EdgeRequestBody(
     val fromId: Int,
     val toId: Int
 )
 
 @Service
 class EdgeService(private val edgeRepository: EdgeRepository) {
-
-    fun createEdge(request: CreateEdgeRequestBody) {
+    fun createEdge(request: EdgeRequestBody) {
         edgeRepository.insertEdge(request.fromId, request.toId)
     }
+
+    fun deleteEdge(request: EdgeRequestBody) {
+        if(!edgeRepository.deleteEdge(request.fromId, request.toId)) {
+            throw EdgeNotFoundException(message = "No edge found with fromId = ${request.fromId} and toId = ${request.toId}.")
+        }
+    }
+
+    fun fetchTreeWithRoot(nodeId: Int): List<Pair<Int, Int>> = edgeRepository.getTreeWithRoot(nodeId)
 }
 
 @Repository
@@ -54,6 +98,67 @@ class EdgeRepository(private val dslContext: DSLContext) {
             throw EdgeAlreadyExistsException(message = "Edge fromId = $fromId, toId = $toId already exists.")
         }
     }
+
+    /**
+     * Given the fact that for this exercise, the connection between companies is represented by a tree, it is assumed that
+     * a company ID can't appear twice in the tree. That beeing said, if a node is removed following the deletion of an edge,
+     * the subtree of the node will be deleted as well unless specified. Reasons for not deleting the entire subtree are
+     * that in real-life cases, the conections between all the downstream companies might still exist even after the deletion of
+     * the upstream company.
+     * @return whether or not a row was deleted
+     */
+    fun deleteEdge(fromId: Int, toId: Int): Boolean {
+        val cteName = "edges_to_delete"
+        val edgesToDeleteCteName: Name = name(cteName)
+
+        val edgesToDeleteCommonTableExpression = edgesToDeleteCteName
+            .fields(EDGE.FROM_ID.name, EDGE.TO_ID.name).`as`(
+                // Base case: the edge to delete
+                select(EDGE.FROM_ID, EDGE.TO_ID)
+                    .from(EDGE)
+                    .where(EDGE.FROM_ID.eq(fromId).and(EDGE.TO_ID.eq(toId)))
+                    .unionAll(
+                        // Recursive case: find child edges
+                        select(EDGE.FROM_ID, EDGE.TO_ID)
+                            .from(EDGE)
+                            .join(edgesToDeleteCteName)
+                            .on(
+                                EDGE.FROM_ID.eq(
+                                    field(name(cteName, EDGE.TO_ID.name), INTEGER)
+                                )
+                            )
+                    )
+            )
+        // Execute the DELETE query
+        val result = dslContext.withRecursive(edgesToDeleteCommonTableExpression)
+            .delete(EDGE)
+            .where(
+                row(EDGE.FROM_ID, EDGE.TO_ID).`in`(
+                    select(EDGE.FROM_ID, EDGE.TO_ID).from(edgesToDeleteCteName)
+                )
+            ).execute()
+        return result > 0
+    }
+
+    fun getTreeWithRoot(nodeId: Int): List<Pair<Int, Int>> {
+        val cteName = "tree"
+        val cte = name(cteName).fields(EDGE.FROM_ID.name, EDGE.TO_ID.name).`as`(
+            select(EDGE.FROM_ID, EDGE.TO_ID).from(EDGE).where(EDGE.FROM_ID.eq(nodeId))
+                .unionAll(
+                    select(EDGE.FROM_ID, EDGE.TO_ID)
+                        .from(table(name(cteName)))
+                        .join(EDGE)
+                        .on(EDGE.FROM_ID.eq(field(name(cteName, EDGE.TO_ID.name), INTEGER)))
+                )
+        )
+        val fetchResult = dslContext.withRecursive(cte).selectFrom(cte).fetch()
+        if (fetchResult.isEmpty()) {
+            throw EdgeNotFoundException(message = "No edge found with fromId = $nodeId.")
+        }
+        return fetchResult.map { it.value1()!! to it.value2()!! }
+    }
+
 }
 
 class EdgeAlreadyExistsException(message: String) : RuntimeException(message)
+class EdgeNotFoundException(message: String) : RuntimeException(message)
--- src/test/kotlin/fr/batgard/prewave_assignment/edge/EdgeControllerTest.kt
+++ src/test/kotlin/fr/batgard/prewave_assignment/edge/EdgeControllerTest.kt
@@ -1,12 +1,16 @@
 package fr.batgard.prewave_assignment.edge
 
 import fr.batgard.prewave_assignment.db.models.tables.Edge.Companion.EDGE
+import fr.batgard.prewave_assignment.db.models.tables.records.EdgeRecord
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.postgresql.ds.PGSimpleDataSource
 import org.springframework.http.HttpStatus
+import kotlin.test.assertNotEquals
 
 class EdgeControllerTest {
     private val testDataSource = PGSimpleDataSource().apply {
@@ -16,24 +20,33 @@ class EdgeControllerTest {
         portNumbers = intArrayOf(5432)
     }
 
+    private val dslContext = DSL.using(testDataSource, SQLDialect.POSTGRES)
+
+
+    @BeforeEach
+    fun cleanUpDatabase() {
+        dslContext.truncate(EDGE).execute()
+    }
+
     @Test
     fun `Given edge doesn't exists When creating it Then it gets inserted in database`() {
-        val dslContext = DSL.using(testDataSource, SQLDialect.POSTGRES)
         val edgeController = EdgeController(EdgeService(EdgeRepository(dslContext)))
 
-        val edgeRequestContent = CreateEdgeRequestBody(fromId = 1, toId = 2)
+        val edgeRequestContent = EdgeRequestBody(fromId = 1, toId = 2)
         edgeController.createEdge(edgeRequestContent)
-        assertEquals(edgeRequestContent, dslContext.fetchOne(EDGE)?.into(CreateEdgeRequestBody::class.java))
+        assertEquals(edgeRequestContent, dslContext.fetchOne(EDGE)?.into(EdgeRequestBody::class.java))
     }
 
     @Test
     fun `Given edge already exists When trying to insert it again Then there's only one entry in db for it`() {
-        val dslContext = DSL.using(testDataSource, SQLDialect.POSTGRES)
         val edgeController = EdgeController(EdgeService(EdgeRepository(dslContext)))
 
-        val edgeRequestContent = CreateEdgeRequestBody(fromId = 1, toId = 2)
+        val edgeRequestContent = EdgeRequestBody(fromId = 1, toId = 2)
         edgeController.createEdge(edgeRequestContent)
-        edgeController.createEdge(edgeRequestContent)
+        try {
+            edgeController.createEdge(edgeRequestContent)
+        } catch (ignore: EdgeAlreadyExistsException) {
+        }
         assertEquals(
             1,
             dslContext.fetchCount(EDGE, EDGE.FROM_ID.eq(1).and(EDGE.TO_ID.eq(2)))
@@ -41,14 +54,88 @@ class EdgeControllerTest {
     }
 
     @Test
-    fun `Given edge already exists When trying to insert it again Then conflict is returned`() {
-        val dslContext = DSL.using(testDataSource, SQLDialect.POSTGRES)
+    fun `Given edge already exists When trying to insert it again Then a edge already exists exception is thrown`() {
         val edgeController = EdgeController(EdgeService(EdgeRepository(dslContext)))
 
-        val edgeRequestContent = CreateEdgeRequestBody(fromId = 1, toId = 2)
+        val edgeRequestContent = EdgeRequestBody(fromId = 1, toId = 2)
         edgeController.createEdge(edgeRequestContent)
-        val responseEntity = edgeController.createEdge(edgeRequestContent)
-        assertEquals(HttpStatus.CONFLICT, responseEntity.statusCode)
-//        println(dslContext.selectFrom(EDGE).fetch())
+        assertThrows<EdgeAlreadyExistsException> {
+            edgeController.createEdge(edgeRequestContent)
+        }
     }
+
+
+    @Test
+    fun `Given edge 0 does not exist, When requesting it with its subtree Then an error not found is returned`() {
+        val edgeController = EdgeController(EdgeService(EdgeRepository(dslContext)))
+
+        insertTreeInDatabase(edgeController)
+
+        assertThrows<EdgeNotFoundException> {
+            edgeController.getTree(rootNodeId = 0)
+        }
+    }
+
+    @Test
+    fun `Given edge 1 exists and is the root node, When requesting it with its subtree Then ok response is returned with the complete list of edges of the entire tree`() {
+        val edgeController = EdgeController(EdgeService(EdgeRepository(dslContext)))
+
+        insertTreeInDatabase(edgeController)
+
+        val entireTree = edgeController.getTree(rootNodeId = 1)
+
+        assertEquals(HttpStatus.OK, entireTree.statusCode)
+        assertEquals(listOf(Pair(1, 2), Pair(1, 3), Pair(2, 4), Pair(2, 5), Pair(3, 6), Pair(5, 7)), entireTree.body)
+    }
+
+    @Test
+    fun `Given edge 2 exists and is not the root node, When requesting it with its subtree Then only its edges and those from it's subtree are returned`() {
+        val edgeController = EdgeController(EdgeService(EdgeRepository(dslContext)))
+
+        insertTreeInDatabase(edgeController)
+
+        val entireTree = edgeController.getTree(rootNodeId = 2)
+
+        assertEquals(listOf(Pair(2, 4), Pair(2, 5), Pair(5, 7)), entireTree.body)
+    }
+
+    @Test
+    fun `Given edge 1 to 5 doesn't exist, When requesting to delete it Then edge not found exception is thrown`() {
+        val edgeController = EdgeController(EdgeService(EdgeRepository(dslContext)))
+        insertTreeInDatabase(edgeController)
+        assertThrows<EdgeNotFoundException> {
+            edgeController.deleteEdge(EdgeRequestBody(1, 5))
+        }
+    }
+
+    @Test
+    fun `Given edge 1 to 3 exists, When requesting to delete it Then response ok is returned`() {
+        val edgeController = EdgeController(EdgeService(EdgeRepository(dslContext)))
+        insertTreeInDatabase(edgeController)
+        assertEquals(HttpStatus.OK, edgeController.deleteEdge(EdgeRequestBody(1, 3)).statusCode)
+    }
+
+    @Test
+    fun `Given edge 1 to 3 exists, When requesting to delete it Then it is removed from db as well as all its subtree`() {
+        val edgeController = EdgeController(EdgeService(EdgeRepository(dslContext)))
+        insertTreeInDatabase(edgeController)
+        edgeController.deleteEdge(EdgeRequestBody(1, 3))
+        val updatedDb = dslContext.fetch(EDGE)
+        val deletedEdges = listOf(EdgeRecord(1, 3), EdgeRecord(3, 6))
+        updatedDb.forEach {
+            assertNotEquals(deletedEdges[0], it)
+            assertNotEquals(deletedEdges[1], it)
+        }
+    }
+
+    private fun insertTreeInDatabase(edgeController: EdgeController) {
+        edgeController.createEdge(EdgeRequestBody(fromId = 1, toId = 2))
+        edgeController.createEdge(EdgeRequestBody(fromId = 1, toId = 3))
+        edgeController.createEdge(EdgeRequestBody(fromId = 2, toId = 4))
+        edgeController.createEdge(EdgeRequestBody(fromId = 2, toId = 5))
+        edgeController.createEdge(EdgeRequestBody(fromId = 3, toId = 6))
+        edgeController.createEdge(EdgeRequestBody(fromId = 5, toId = 7))
+    }
+
+
 }
