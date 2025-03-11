53f167bec7c5428267625b44111b4a7c385f85ed
Remi Gatard <remi.gatard@runtastic.com>
2025-03-11 15:32:44 +0100

    Remove sample code

--- src/main/kotlin/fr/batgard/prewave_assignment/PrewaveAssignmentApplication.kt
+++ src/main/kotlin/fr/batgard/prewave_assignment/PrewaveAssignmentApplication.kt
@@ -1,25 +1,10 @@
 package fr.batgard.prewave_assignment
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class PrewaveAssignmentApplication {
-	@Bean
-	fun commandLineRunner(ctx: ApplicationContext): CommandLineRunner {
-		return CommandLineRunner {
-			println("Let's inspect the beans provided by Spring Boot:")
-			val beanNames = ctx.beanDefinitionNames
-			beanNames.sort()
-			beanNames.onEach {
-				println(it)
-			}
-		}
-	}
-}
+class PrewaveAssignmentApplication {}
 
 fun main(args: Array<String>) {
 	runApplication<PrewaveAssignmentApplication>(*args)
